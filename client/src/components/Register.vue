dad22d8e5d99f920b2c257c40251793ec32d8a34
Jacob Barats <jacobbarats@Jacobs-MacBook-Pro.local>
2023-11-07 15:48:33 -0500

    added vuetify on the front end. On the back end added more layers to the routing, using joi for schema configuration

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,19 +1,11 @@
-<!-- <script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
-</script> -->
-
 <script>
 import AuthenticationService from '@/services/AuthenticationService'
 export default {
   data () {
     return {
       email: 'abc',
-      password: ''
+      password: '',
+      error: null
     }
   },
   watch: {
@@ -28,20 +20,31 @@ export default {
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
+      try {
+        await AuthenticationService.register({
         email: this.email,
         password: this.password
-      })
-      console.log(response.data)
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }   
   }
 }
 </script>
 
 <template>
-  <div >
-    <h1>Register</h1>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class = "cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+      </div>
+    </v-flex>
+  </v-layout>
 
+  <div class="pl-4 pr-4 pt-2 pb-2">
     <input 
       type = "email" 
       name = "email"
@@ -54,11 +57,17 @@ export default {
       v-model = "password"
       placeholder = "password" />
       <br />
-      <button 
-      @click = "register"> Register </button>
+      <div class="error" v-html="error" />
+      <v-btn 
+        class="cyan"
+        @click = "register"> 
+        Register 
+  </v-btn>
   </div>
 </template>
 
 <style scoped>
-
+.error {
+  color: red;
+}
 </style>
--- server/src/app.js
+++ server/src/app.js
@@ -4,22 +4,20 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 
+const {sequelize} = require('./models')
+const config = require('./config/config')
+
 const app = express() //builds an express server
 app.use(morgan('combined')) //log generator combine certain printing arrangement
 app.use(bodyParser.json()) //easily parses any json requests sent it
 app.use(cors()) //allows for easy client access Enabling is a security risk
 
-app.get('/status', (req,res) => {
-  res.send({
-    message: 'hello world!'
-  })
-})
+require('./routes')(app)
 
-//res appears to be a return address. Req will have the content of the sent data
-app.post('/register', (req, res) => {
-  res.send({
-    message: `Hello ${req.body.email} user was registered! Have fun!`
-  })
-})
 
-app.listen(process.env.PORT || 8081)
+console.log(typeof sequelize)
+sequelize.sync({force: false}) //connects the databse
+  .then(() => {
+    app.listen(process.env.PORT || 8081)
+    console.log(`Server started on ${config.port}`)
+  })  
